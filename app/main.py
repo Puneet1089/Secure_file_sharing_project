2391ab19cc2afb1d3a7c9b8e8007237817a153eb
Puneet Bhardwaj <145178897+Puneet1089@users.noreply.github.com>
2026-07-04 00:13:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,66 +1,100 @@
+# to get current date & time & calculating the expiry time
 from datetime import datetime, timedelta
 
+# to import fastapi
 from fastapi import FastAPI, UploadFile, File, Form, Depends, HTTPException
+
+# used for sending file back to the user for download
 from fastapi.responses import FileResponse
 
+# db session
 from sqlalchemy.orm import Session
 
+# import db dependency
 from app.database import get_db
+
+# import file model (changed the name beacuse of conflict with fastapi file)
 from app.models import File as FileModel
+
+# reesponse schema
 from app.schemas import UploadResponse
+
+# helper functions
 from app.utils import generate_file_id, save_uploaded_file
+
+# db operations
 from app.crud import (
     save_file_record,
     get_file_record,
     update_download_count,
 )
 
+# creating fastapi application
 app = FastAPI(
     title="Secure File Sharing System",
-    description="A backend application to upload files and share them securely using temporary download links.",
+    description="An application to upload files and generate secure links.",
     version="1.0"
 )
 
+# home api
+
+# simple api for checking whether server is running or not
 @app.get("/")
 def home():
     return {
-        "message": "Secure File Sharing System is Running"
+        "message":"Secure File Sharing System is Running"
     }
 
-@app.post("/upload", response_model=UploadResponse)
-def upload_file(
-    file: UploadFile = File(...),
-    expiry_hours: int = Form(...),
-    max_downloads: int = Form(...),
-    db: Session = Depends(get_db)
-):
+# upload api
 
-    # Validation
-    if expiry_hours <= 0:
+@app.post("/upload",response_model=UploadResponse)
+def upload_file(
+    # uploaded file
+    file: UploadFile=File(...),
+
+    # link expiry time (in hours)
+    expiry_hours:int =Form(...),
+
+    # max downloads allowed
+    max_downloads: int= Form(...),
+
+    # database session
+    db:Session= Depends(get_db)
+):
+    # input validation
+
+    # expiry time cannot be zero or negative
+    if expiry_hours<= 0:
         raise HTTPException(
             status_code=400,
             detail="Expiry hours must be greater than 0."
         )
-
-    if max_downloads <= 0:
+    
+    # download limit cannot be zero or negative
+    if max_downloads<=0:
         raise HTTPException(
             status_code=400,
             detail="Max downloads must be greater than 0."
         )
 
-    # Generate unique file id
-    file_id = generate_file_id()
+    # Generating unique link
 
-    # Save uploaded file
-    file_path = save_uploaded_file(file, file_id)
+    # Creating random UUID link 
+    file_id =generate_file_id()
+    # Save file in uploads folder
+    file_path= save_uploaded_file(file, file_id)
 
-    # Current time
-    uploaded_at = datetime.now()
+    # Time Info
 
-    # Calculate expiry
-    expiry_time = uploaded_at + timedelta(hours=expiry_hours)
+    # Current time upload
+    uploaded_at=datetime.now()
 
-    # Create database object
+    # calculating expiry time of the link
+    expiry_time =uploaded_at+timedelta(hours=expiry_hours)
+
+    # Creating Database Object
+
+    # create 1 database record
     file_record = FileModel(
         file_id=file_id,
         filename=file.filename,
@@ -71,49 +105,57 @@ def upload_file(
         downloads_left=max_downloads
     )
 
-    # Save metadata
-    save_file_record(db, file_record)
+    # Saving metadata in Mysql
 
+    save_file_record(db,file_record)
+
+    # Return Response with the file id
     return UploadResponse(
         message="File uploaded successfully.",
-        file_id=file_id,
-        download_link=f"http://127.0.0.1:8000/download/{file_id}"
+        file_id = file_id,
+        download_link= f"http://127.0.0.1:8000/download/{file_id}"
     )
 
+# Download API
 @app.get("/download/{file_id}")
 def download_file(
-    file_id: str,
-    db: Session = Depends(get_db)
+
+    # receving the file_id from url
+    file_id:str,
+    # Database session
+    db: Session= Depends(get_db)
+
 ):
+    # Searching the file in database
+    file_record =get_file_record(db, file_id)
 
-    # Get file from database
-    file_record = get_file_record(db, file_id)
-
-    # File not found
+    # if file doesnot exist
     if file_record is None:
         raise HTTPException(
             status_code=404,
             detail="File not found."
         )
+    # Checking download link is valid or not
+    if datetime.now() >file_record.expiry_time:
 
-    # Check expiry
-    if datetime.now() > file_record.expiry_time:
         raise HTTPException(
-            status_code=403,
-            detail="Download link has expired."
-        )
 
-    # Check download limit
-    if file_record.downloads_left <= 0:
+            status_code=403,
+
+            detail="Download link has expired."
+
+        )
+    # Checking the download limit
+    if file_record.downloads_left<= 0:
+
         raise HTTPException(
             status_code=403,
             detail="Download limit reached."
         )
-
-    # Update remaining downloads
+    # Reducing download count
     update_download_count(db, file_record)
 
-    # Return file
+    # Sending the file back to the user
     return FileResponse(
         path=file_record.filepath,
         filename=file_record.filename
