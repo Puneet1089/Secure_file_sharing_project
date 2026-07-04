bc969a30483f4020a5c7f8021ad4fbe646cccca1
Puneet Bhardwaj <145178897+Puneet1089@users.noreply.github.com>
2026-07-04 08:11:25 +0530

    Update schemas.py
    Added comments to clarify the purpose of the UploadResponse fields.

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,8 @@
+# import basemodel from pydantic
 from pydantic import BaseModel
 
-
+# response format for upload api (structure)
 class UploadResponse(BaseModel):
-    message: str
-    file_id: str
-    download_link: str
+    message: str   # Success message
+    file_id: str   # Unique file id
+    download_link: str   # Download URL
