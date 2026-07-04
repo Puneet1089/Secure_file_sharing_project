3e97da88306a8b4f857d9aae36b217f3bd4ef1bc
Puneet Bhardwaj <145178897+Puneet1089@users.noreply.github.com>
2026-07-04 08:11:01 +0530

    Update crud.py
    Added comments to clarify database operations in CRUD functions.

--- app/crud.py
+++ app/crud.py
@@ -1,24 +1,24 @@
+# import session to perform db operations
 from sqlalchemy.orm import Session
+# import file model(files table)
 from app.models import File
 
-
+# saving a new file record into our db
 def save_file_record(db: Session, file):
-    db.add(file)
-    db.commit()
-    db.refresh(file)
-    return file
+    db.add(file)   # add object in db session
+    db.commit()   # save changes permanently
+    db.refresh(file)   # refresh object to get latest value from db
+    return file   # return saved object
 
-
+# get file details using unique file_id
 def get_file_record(db: Session, file_id: str):
-    return db.query(File).filter(File.file_id == file_id).first()
+    return db.query(File).filter(File.file_id ==file_id).first()   # search the files table and return first matching record
 
-
+# reduce remaining download count
 def update_download_count(db: Session, file):
-    if file.downloads_left > 0:
-        file.downloads_left -= 1
+    if file.downloads_left > 0:   #  if downloads are avialble then decerease
+        file.downloads_left-=1
 
-    db.commit()
-    db.refresh(file)
-
+    db.commit()   # save updated value
+    db.refresh(file)   # refresh object with latest db values
     return file
-
