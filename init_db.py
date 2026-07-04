539ce079f0c0daa292a132242f4ee2fe0f109018
Puneet Bhardwaj <145178897+Puneet1089@users.noreply.github.com>
2026-07-04 08:11:48 +0530

    Update init_db.py
    Added comment to clarify table creation process.

--- init_db.py
+++ init_db.py
@@ -1,6 +1,7 @@
+# import db engine & base clas
 from app.database import Base, engine
+# import file model such that sqlalchemy will know which table to create
 from app.models import File
 
-Base.metadata.create_all(bind=engine)
-
+Base.metadata.create_all(bind=engine)    # creating all tables defined in models.py file
 print("Database tables created successfully!")
