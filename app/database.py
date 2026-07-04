c4b6919b71a623fc342dd8178cd676765c44876a
Puneet Bhardwaj <145178897+Puneet1089@users.noreply.github.com>
2026-07-04 08:23:23 +0530

    Update database.py
    Added comments for better understanding of the code.

--- app/database.py
+++ app/database.py
@@ -1,12 +1,17 @@
+# import function to create db connection
 from sqlalchemy import create_engine
+# import base class & session creator from sqlalchemy
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+# import dotenv to read environment variables from .env file
 from dotenv import load_dotenv
+# import os module to access environment variables
 import os
 
-# Load environment variables
+# load all variables from .env file
 load_dotenv()
 
+# creating db connection string using values from .env
 DATABASE_URL = (
     f"mysql+mysqlconnector://"
     f"{os.getenv('DB_USER')}:"
@@ -16,23 +21,23 @@ DATABASE_URL = (
     f"{os.getenv('DB_NAME')}"
 )
 
-# Create database engine
+# creating connection engine with mysql db
 engine = create_engine(DATABASE_URL)
 
-# Session
+# creating a session factory for db operations
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base class
+# creating a base class that will be used by all db models
 Base = declarative_base()
 
-# Database Dependency
+# creating a db session for every request
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal()     # open db session
     try:
-        yield db
+        yield db    # give session to api
     finally:
-        db.close()
+        db.close()   # always close db connection using finally
