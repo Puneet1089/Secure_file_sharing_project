a6342f4ae255aedb9b5861fd95c84f4f1c2589b8
Puneet Bhardwaj <145178897+Puneet1089@users.noreply.github.com>
2026-07-04 08:22:58 +0530

    Update utils.py
    Updated comments for clarity and consistency.

--- app/utils.py
+++ app/utils.py
@@ -1,35 +1,37 @@
+# import os module for file and folder operations
 import os
+# import uuid module to generate unique id
 import uuid
 
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = "uploads"    # folder where uploaded files will be stored
 
-# Create uploads folder if it doesn't exist
+# creating uploads folder if it not exist
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-
+# generating a unique id for every uploaded file
 def generate_file_id():
     """
     Generate a unique ID for every uploaded file.
     """
-    return str(uuid.uuid4())
+    return str(uuid.uuid4())    # return random uuid as string
 
-
+# saving uploaded file inside uploads folder
 def save_uploaded_file(upload_file, file_id):
     """
     Save uploaded file inside uploads folder.
     """
 
-    # Get file extension
+    # get original file extension (like.pdf)
     extension = os.path.splitext(upload_file.filename)[1]
 
-    # Create unique filename
+    # creating unique filename using uuid
     new_filename = f"{file_id}{extension}"
 
-    # Full file path
+    # creating complete file path
     file_path = os.path.join(UPLOAD_FOLDER, new_filename)
 
-    # Save file
+    # saving uploaded file to my local storage
     with open(file_path, "wb") as file:
         file.write(upload_file.file.read())
 
-    return file_path
+    return file_path    # return saved file path
